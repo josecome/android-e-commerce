6a2dfa94491de2b068ca6e91e8114eae11e048e9
Jose Jaime Come <josejaimecome@gmail.com>
2023-11-02 19:49:35 +0200

    Minor changes made

---
+++ app/src/main/java/com/example/android_e_commerce/ListData.java
@@ -0,0 +1,14 @@
+package com.example.android_e_commerce;
+
+public class ListData {
+    String nametype, name, description;
+    double price;
+    int image;
+    public ListData(String nametype, String name, String description, double price, int image) {
+        this.nametype = nametype;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+    }
+}
